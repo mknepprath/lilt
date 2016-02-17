bd1f3b55eb933ef51e8ac996d343e1b618785e5a
Michael Knepprath <mknepprath@gmail.com>
2016-02-17 13:34:59 -0600

    just print status id

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     for status in tweepy.Cursor(twitter.api.user_timeline).items():
         try:
-            print status
+            print status.id
         except:
             pass
 
