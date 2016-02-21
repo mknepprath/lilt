011fa1fd931cf4cced97e2caa3db7d8243250cc9
Michael Knepprath <mknepprath@gmail.com>
2016-02-20 19:59:36 -0600

    trying to fetch test table

--- bot.py
+++ bot.py
@@ -18,9 +18,10 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
-cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+cur.execute("SELECT * FROM test;")
+cur.fetchone()
 
+print cur.fetchone()
 print cur
 
 class TwitterAPI:
