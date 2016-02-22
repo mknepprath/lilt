34d947378f2d76be617421b87b62a90b1d27e34e
Michael Knepprath <mknepprath@gmail.com>
2016-02-22 17:50:17 -0600

    attempt to print tweet id

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
             users = cur.fetchall()
             print users
             print users[0]
-            print users[0].id
+            print users[0][2]
 
             #for user in users:
             #    try:
