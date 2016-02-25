65fecac3759ff46afa5cc110c4d2cd767b48ea5e
Michael Knepprath <mknepprath@gmail.com>
2016-02-25 09:50:37 -0600

    added a bunch of comments to clarify things

--- bot.py
+++ bot.py
@@ -47,6 +47,7 @@ class TwitterAPI:
 if __name__ == "__main__":
     twitter = TwitterAPI()
 
+    # deletes all tweets so far
     #for status in tweepy.Cursor(twitter.api.user_timeline).items():
     #    try:
     #        print status.text
@@ -54,13 +55,18 @@ if __name__ == "__main__":
     #    except:
     #        pass
 
+    # init mentions
     mentions = []
 
+    # go through mentions from Twitter using Tweepy
     for mention in tweepy.Cursor(twitter.api.mentions_timeline).items():
         try:
+            # splits tweet at first space, game_name = @familiarlilt
             game_name, tweet = (mention.text).split(" ",1)
 
+            # init mentioned
             mentioned = False
+            # runs through mentions and notes if current user has been mentioned
             for m in mentions:
                 try:
                     if mention.user.id == m['user_id']:
@@ -68,6 +74,7 @@ if __name__ == "__main__":
                 except:
                     pass
 
+            # if user was already added, don't append it to mentions again
             if mentioned != True:
                 mentions.append({
                     'screen_name': mention.user.screen_name,
@@ -86,26 +93,33 @@ if __name__ == "__main__":
             tweet = mention['tweet']
             tweetid = mention['tweetid']
 
+            # ... this might not be necessary. assigns user data to users
             cur.execute("SELECT * FROM users")
             users = cur.fetchall()
 
+            # attempts to grab current user from users table
             cur.execute("""SELECT 1 FROM users WHERE name = %s;""", (screen_name,))
             user_exists = cur.fetchone()
+            # if they're in the table, grab tweet id from table
             if user_exists != None:
                 print "current player"
                 cur.execute("""SELECT 1 FROM users WHERE last_tweet_id = %s;""", (str(tweetid),))
                 tweet_exists = cur.fetchone()
+                # if tweetid isn't in users table, update tweetid
                 if tweet_exists == None:
                     print "new tweet"
                     cur.execute("UPDATE users SET last_tweet_id = %s WHERE name = %s;", (tweetid, screen_name))
                     #conn.commit()
+                # otherwise, do nothing - tweet has already been replied to
                 else:
                     print "old tweet"
             else:
+                # if user is not in the users table, add user and tweetid
                 print "new player"
                 cur.execute("INSERT INTO users (name, id, last_tweet_id) VALUES (%s, %s, %s)", (screen_name, user_id, tweetid))
                 #conn.commit()
 
+            # the below stuff should get moved into if statements above
             print tweet
 
             randstring = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
