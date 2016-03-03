796ef78a33ff8b7177d0895f1ea04e2d9b6c6c39
Michael Knepprath <mknepprath@gmail.com>
2016-03-03 02:15:08 -0600

    dropping some bananas

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         mentions.append({
             'screen_name': 'mknepprath',
             'user_id': 15332057,
-            'tweet': 'pick up banana', # update this with tweet to test
+            'tweet': 'drop banana', # update this with tweet to test
             'tweetid': 703619369989853185
         })
 
