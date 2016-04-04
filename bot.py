d4bb21eecdeaca44273e63547a38bc7d4b0b525f
Michael Knepprath <mknepprath@gmail.com>
2016-04-04 00:55:26 -0500

    print test now that there is not check

--- bot.py
+++ bot.py
@@ -329,15 +329,12 @@ if __name__ == "__main__":
                 # if there is a trigger, assing json to trigger
                 if (trig != None) and (trig[0] != None):
                     trigger = json.loads(trig[0])
-                    print "trigger: " + str(trigger)
-                    # if trigger isn't in events, add it
-                    if trigger not in events[position]:
-                        print "test"
-                        events[position].append(trigger)
-                        print "events: " + str(events)
-                        # update events
-                        cur.execute("UPDATE users SET events = %s WHERE id = %s;", (json.dumps(events), str(user_id),))
-                        conn.commit()
+                    print "test"
+                    events[position].append(trigger)
+                    print "events: " + str(events)
+                    # update events
+                    cur.execute("UPDATE users SET events = %s WHERE id = %s;", (json.dumps(events), str(user_id),))
+                    conn.commit()
                 print "trigger: " + str(trigger)
 
                 # get condition
