62e99bba5cc26fb8745e7285d5fc7e49254cad41
Michael Knepprath <mknepprath@gmail.com>
2016-04-10 15:40:11 -0500

    attempt to add musical notes to rstring

--- bot.py
+++ bot.py
@@ -293,7 +293,7 @@ def mbuild(screen_name, message):
 error_message = ["You can't do that.", "That can't be done.", "Didn't work.", "Oops, can't do that.", "Sorry, you can't do that.", "That didn't work.", "Try something else.", "Sorry, you'll have to try something else.", "Oops, didn't work.", "Oops, try something else.", "Nice try, but you can't do that.", "Nice try, but that didn't work.", "Try something else, that didn't seem to work."]
 
 # rstring to avoid Twitter getting mad about duplicate tweets // should think up a better solution for this
-rstring = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+rstring = ''.join(random.choice(string.ascii_uppercase + string.digits + u'\u2669' + u'\u266A' + u'\u266B' + u'\u266C') for _ in range(5))
 print 'String of random characters created.'
 
 if __name__ == "__main__":
@@ -391,6 +391,7 @@ if __name__ == "__main__":
                     conn.commit()
                     reply = True
                 else:
+                    # this reply is purely for debugging - since reply defaults to True, this would be redundant
                     reply = False
                     print 'This person isn\'t playing Lilt.'
 
