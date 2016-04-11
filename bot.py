16b287d88de61759ae963228f56980bc17e197a5
Michael Knepprath <mknepprath@gmail.com>
2016-04-11 16:57:20 -0500

    refactored dbselect - simplified it since it will never get as complicated as I had it

--- bot.py
+++ bot.py
@@ -285,11 +285,11 @@ def storeerror(move, position):
         conn.commit()
     return "Stored the failed attempt for future reference."
 
-def dbselect(col1, table, col2, val2, col3=None, val3=None, col4=None, val4=None):
-    if (col3 != None) and (col4 != None):
-        cur.execute("SELECT " + col1 + " FROM " + table + " WHERE " + col2 + " = %s AND " + col3 + " = %s AND " + col4 + " = %s;", (val2,val3,val4))
-    elif col3 != None:
-        cur.execute("SELECT " + col1 + " FROM " + table + " WHERE " + col2 + " = %s AND " + col3 + " = %s;", (val2,val3))
+def dbselect(col1, table, col2, val2, position=None, condition=None):
+    if condition != None:
+        cur.execute("SELECT " + col1 + " FROM moves WHERE move = %s AND position = %s AND condition = %s;", (val2,position,condition))
+    elif position != None:
+        cur.execute("SELECT " + col1 + " FROM moves WHERE move = %s AND position = %s AND condition IS NULL;", (val2,position))
     else:
         cur.execute("SELECT " + col1 + " FROM " + table + " WHERE " + col2 + " = %s;", (val2,))
     o = cur.fetchone()
