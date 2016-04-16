b43b369778f807e3892331508144eedbbd936fc8
Michael Knepprath <mknepprath@gmail.com>
2016-04-16 15:02:08 -0500

    printing first letter of 1st word in tweet

--- bot.py
+++ bot.py
@@ -323,9 +323,10 @@ if __name__ == "__main__":
             reply = True if debug == True else False
 
             # splits tweet at first space, game_name = @familiarlilt (this should probably happen in the next loop)
-            tweet_raw = '' if len((text).split()) == 1 else (text).split(' ',1)[1]
-            if (tweet_raw).split(' ',1)[0][0] == '@':
-                tweet = (tweet_raw).split(' ',1)[1]
+            tweet = '' if len((text).split()) == 1 else (text).split(' ',1)[1]
+            print (tweet).split(' ',1)[0][0]
+            if (tweet).split(' ',1)[0][0] == '@':
+                tweet = (tweet).split(' ',1)[1]
                 move = cleanstr(tweet)
             else:
                 move = cleanstr(tweet)
