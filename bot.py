6f0aa423272101189c5556c5915458a699f60473
Michael Knepprath <mknepprath@gmail.com>
2016-04-16 21:31:14 -0500

    print mention_name to get to the bottom of this...

--- bot.py
+++ bot.py
@@ -300,6 +300,7 @@ if __name__ == "__main__":
                 mentioned = False
                 for m in mentions:
                     mention_name = (mention['text']).split(' ',1)[0].lower()
+                    print mention_name
                     if (mention['user_id'] == m['user_id']) or (mention_name != '@familiarlilt'): # if user matches user already in mentions and if sent directly to Lilt
                         mentioned = True
                 if mentioned == False: # if user hasn't been mentioned, append it to mentions
