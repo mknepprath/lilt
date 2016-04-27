5e4faf67bb4759fa894d51608a4c5aaaca68e8b1
Michael Knepprath <mknepprath@gmail.com>
2016-04-26 19:22:52 -0500

    added pass after print in log function, so function is exited after completion

--- bot.py
+++ bot.py
@@ -254,6 +254,7 @@ def log(s):
         cur.execute("INSERT INTO console (log, time) VALUES (%s, 'now')", (str(s),))
         conn.commit()
         print str(s)
+        pass
     else:
         pass
 
