2432490e781c2f196488fad4caac2ff330e27f1e
Michael Knepprath <mknepprath@gmail.com>
2016-04-28 11:03:08 -0500

    testing dbselect to see what it's doing

--- db.py
+++ db.py
@@ -20,6 +20,7 @@ def dbselect(col1, table, col2, val, position=None, condition=None):
     print position
     print condition
     if condition != None:
+        print 'should run this..'
         cur.execute("SELECT " + col1 + " FROM " + table + " WHERE move = %s AND position = %s AND condition = %s;", (val,position,json.dumps(condition)))
     elif position != None:
         cur.execute("SELECT " + col1 + " FROM " + table + " WHERE move = %s AND position = %s AND condition IS NULL;", (val,position))
