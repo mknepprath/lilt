ba75bd3bd99c347dec22bf62c858bb006820fd55
Michael Knepprath <mknepprath@gmail.com>
2016-04-29 09:34:14 -0500

    added 'b' back to get, needed for checking if valid

--- command.py
+++ command.py
@@ -3,19 +3,15 @@ from db import dbselect
 from utils import cleanstr
 
 def get(tweet):
-    print '1'
     if len((tweet).split()) >= 2:
-        a = (tweet).split(' ',1)[0]
+        a, b = (tweet).split(' ',1)
         a = ''.join(ch for ch in a if ch not in set(string.punctuation)).lower()
         if (a == 'drop'):
             if dbselect('name', 'items', 'name', cleanstr(b)) != None:
                 return a
         elif (a == 'give'):
-            print '2'
             c, d = (b).split(' ',1)
-            print '3'
             if dbselect('name', 'items', 'name', cleanstr(d)) != None:
-                print '4'
                 return a
         elif (a == 'liltadd') and ((user['id'] == '15332057') or (user['id'] == '724754312757272576')):
             return a
