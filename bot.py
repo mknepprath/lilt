978ed6c9faeb72e1867c1785a15a77d339129ddb
Michael Knepprath <mknepprath@gmail.com>
2016-04-29 10:26:47 -0500

    print tweet type of var

--- bot.py
+++ bot.py
@@ -142,6 +142,7 @@ if __name__ == "__main__":
             tweet = '' if len((user['text']).split()) == 1 else (user['text']).split(' ',1)[1]
             if (tweet).split(' ',1)[0][0] == '@':
                 tweet = (tweet).split(' ',1)[1]
+            log(rec, type(tweet))
             move = cleanstr(tweet)
 
             # attempts to grab current user from users table
