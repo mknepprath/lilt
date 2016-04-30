a9b50cea57ef847f9bcd0eeb663427fd24a77da0
Michael Knepprath <mknepprath@gmail.com>
2016-04-30 02:23:47 -0500

    trying events.copy()

--- event.py
+++ event.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 import db
-import copy
 
 def getcurrent(move, position, inventory, events):
-    events_inv = copy.copy(events)
+    events_inv = events.copy()
     items = list(inventory.keys())
     for item in items:
         events_inv[position][item] = 'inventory'
     current_event = None
     for key, value in events_inv[position].iteritems():
-        event = {}
-        event[key] = value
+        e = {}
+        e[key] = value
         # check if there is a response for this move when condition is met (this event)
-        response = db.select('response', 'moves', 'move', move, position, event)
+        response = db.select('response', 'moves', 'move', move, position, e)
         if response != None:
-            current_event = event
+            current_event = e
             break
     return current_event
