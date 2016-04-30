797cf766570f8171c00316e1d6b5b6898f7754fc
Michael Knepprath <mknepprath@gmail.com>
2016-04-29 23:08:54 -0500

    more prints in command give

--- command.py
+++ command.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import string
 import item
-from db import select, delete, newmove
+import db
 from utils import cleanstr, invbuild
 
 def get(tweet, inventory, id, position):
@@ -10,18 +10,21 @@ def get(tweet, inventory, id, position):
         a = ''.join(ch for ch in a if ch not in set(string.punctuation)).lower()
 
         if (a == 'drop'):
-            if select('name', 'items', 'name', cleanstr(b)) != None:
+            if db.select('name', 'items', 'name', cleanstr(b)) != None:
                 return item.drop(cleanstr(b), inventory, id)
 
         elif (a == 'give'):
+            print 'giving'
             c, d = (b).split(' ',1)
             e, f = (d).split(' ',1)
+            print 'after splits'
             if (e == 'the') or (e == 'a') or (e == 'an'):
                 d = cleanstr(f)
             else:
                 d = cleanstr(d)
-            if select('name', 'items', 'name', d) != None:
-                print 'going to return...'
+            print 'before db.select'
+            if db.select('name', 'items', 'name', d) != None:
+                print 'after'
                 return item.give(d, inventory, id, position, ''.join(ch for ch in c if ch not in set(string.punctuation)).lower())
 
         elif (a == 'inventory') or (a == 'check inventory'):
@@ -31,12 +34,12 @@ def get(tweet, inventory, id, position):
                 return invbuild(inventory)
 
         elif (a == 'delete me from lilt') or (a == u'💀💀💀'):
-            delete('users', 'id', id)
+            db.delete('users', 'id', id)
             return 'You\'ve been removed from Lilt. Thanks for playing!'
 
         elif (a == 'liltadd') and ((user['id'] == '15332057') or (user['id'] == '724754312757272576')):
             addmove, addresponse = (b).split('~',1)
-            newmove(addmove, addresponse, position)
+            db.newmove(addmove, addresponse, position)
             return '\'' + addmove + '\' was added to Lilt.'
     else:
         return None
