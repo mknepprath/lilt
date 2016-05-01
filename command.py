263288896d266b5e4eadaa513e4c3cdd153995b4
Michael Knepprath <mknepprath@gmail.com>
2016-05-01 13:14:26 -0500

    added more logs to command, update so it always returns two values

--- command.py
+++ command.py
@@ -13,28 +13,29 @@ def get(tweet, inventory, id, position):
             if cansplit(d):
                 e, f = (d).split(' ',1)
         a = cleanstr(a)
+        db.log(True, 'Split the tweet.')
         if (a == 'drop'):
             if cansplit(b) and ((c == 'the') or (c == 'a') or (c == 'an') or (c == 'some')):
                 b = cleanstr(d)
             else:
                 b = cleanstr(b)
             if db.select('name', 'items', 'name', b) != None:
-                return item.drop(b, inventory, id)
+                return (True, item.drop(b, inventory, id))
         elif (a == 'give') and c: # c must exist for give to work
             if cansplit(d) and ((e == 'the') or (e == 'a') or (e == 'an') or (e == 'some')):
                 d = cleanstr(f)
             else:
                 d = cleanstr(d)
             if db.select('name', 'items', 'name', d) != None:
-                return item.give(d, inventory, id, position, cleanstr(c))
+                return (True, item.give(d, inventory, id, position, cleanstr(c)))
         elif (a == 'inventory') or (a == 'check inventory') or (a == 'what am i holding'):
             if inventory == {}:
-                return 'Your inventory is empty at the moment.'
+                return (True, 'Your inventory is empty at the moment.')
             else:
-                return invbuild(inventory)
+                return (True, invbuild(inventory))
         elif (a == 'delete me from lilt') or (a == u'💀💀💀'):
             db.delete('users', 'id', id)
-            return 'You\'ve been removed from Lilt. Thanks for playing!'
+            return (True, 'You\'ve been removed from Lilt. Thanks for playing!')
         elif (a == 'liltadd') and ((id == '15332057') or (id == '724754312757272576') or (id == '15332062')):
             db.log(True, 'Running liltadd...')
             if len((b).split('~')) >= 2:
