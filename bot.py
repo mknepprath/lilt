5d173ce921928c27354ab25373a5149078c64a49
Michael Knepprath <mknepprath@gmail.com>
2016-05-02 11:21:23 -0500

    removed storeerror

--- bot.py
+++ bot.py
@@ -217,8 +217,7 @@ if __name__ == "__main__":
                     else:
                         db.log(rec, 'I guess that move didn\'t work.')
                         message = mbuild(user['screen_name'], random.choice(error_message))
-                        if debug == False:
-                            db.log(rec, db.storeerror(move, user['position']))
+                        # db.log(rec, db.storeerror(move, user['position']))
                 else:
                     db.log(rec, 'Command acquired, printing reply...')
                     message = mbuild(user['screen_name'], cmd)
