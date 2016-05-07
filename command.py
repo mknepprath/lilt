fec48efac3637e9cbcb66fca3ccca9f847f767e0
Michael Knepprath <mknepprath@gmail.com>
2016-05-07 14:41:34 -0500

    printing in drop function

--- command.py
+++ command.py
@@ -19,7 +19,6 @@ def get(tweet, inventory, id, position):
             item = cleanstr(' '.join(rend[2:len(rend)]))
         else:
             item = cleanstr(' '.join(rend[1:len(rend)]))
-        print db.select('name', 'items', 'name', item)
         if db.select('name', 'items', 'name', item) != None:
             return (True, item.drop(item, inventory, id, quantity=quantity))
     elif (rend[0] == 'give') and (len(rend) >= 3): # give(0) @benlundsten(1) the(2) dawn(3) porter()
