dfb9cee1cd39c7dbf16dcd22b83c3e5a34e4a1c5
Michael Knepprath <mknepprath@gmail.com>
2016-05-07 08:10:21 -0500

    converted all command logic to using rend list instead of a,b,c,d,etc

--- command.py
+++ command.py
@@ -1,53 +1,47 @@
 # -*- coding: utf-8 -*-
 import string
+import re
 import item
 import db
 from utils import cleanstr, invbuild, cansplit
 
 def get(tweet, inventory, id, position):
-    rendtweet = tweet.split()
-    print rendtweet
-    print rendtweet[0]
-    if cansplit(tweet):
-        a, b = (tweet).split(' ',1)
-        if cansplit(b):
-            c, d = (b).split(' ',1)
-            if cansplit(d):
-                e, f = (d).split(' ',1)
-        a = cleanstr(a)
-        cmdtweet = cleanstr(tweet)
-    else:
-        a = cleanstr(tweet)
-        cmdtweet = cleanstr(tweet)
-    if (a == 'drop'):
+    # a = 0, b = 1, c = 1, d = 2, e = 2, f = 3
+    cmd = cleanstr(tweet)
+    print cmd
+    rend = re.sub(r'http\S+', '', tweet).lower().split() # test regex by including a link in tweet
+    print rend
+    if (rend[0] == 'drop') and (len(rend) >= 2): # drop(0) banana(1) # drop(0) the(1) dawn(2) porter(3)
         quantity = None
-        if cansplit(b) and (c == 'all'): # or check if it can be converted to a valid int
+        if (len(rend) >= 3) and (rend[1] == 'all'): # or check if it can be converted to a valid int
             quantity = 'all'
-            b = cleanstr(d)
-        elif cansplit(b) and ((c == 'the') or (c == 'a') or (c == 'an') or (c == 'some')):
-            b = cleanstr(d)
+            item = cleanstr(' '.join(rend[2:len(rend)]))
+        elif (len(rend) >= 3) and ((rend[1] == 'the') or (rend[1] == 'a') or (rend[1] == 'an') or (rend[1] == 'some')):
+            item = cleanstr(' '.join(rend[2:len(rend)]))
         else:
-            b = cleanstr(b)
-        if db.select('name', 'items', 'name', b) != None:
-            return (True, item.drop(b, inventory, id, quantity=quantity))
-    elif (a == 'give') and c: # c must exist for give to work
-        if cansplit(d) and ((e == 'the') or (e == 'a') or (e == 'an') or (e == 'some')):
-            d = cleanstr(f)
+            item = cleanstr(' '.join(rend[1:len(rend)]))
+        if db.select('name', 'items', 'name', item) != None:
+            return (True, item.drop(item, inventory, id, quantity=quantity))
+    elif (rend[0] == 'give') and (len(rend) >= 3): # give(0) @benlundsten(1) the(2) dawn(3) porter()
+        if (len(rend) >= 4) and ((rend[2] == 'the') or (rend[2] == 'a') or (rend[2] == 'an') or (rend[2] == 'some')):
+            item = cleanstr(' '.join(rend[3:len(rend)]))
         else:
-            d = cleanstr(d)
-        if db.select('name', 'items', 'name', d) != None:
-            return (True, item.give(d, inventory, id, position, cleanstr(c)))
-    elif (a == 'inventory') or (cmdtweet == 'check inventory') or (cmdtweet == 'what am i holding'): # multi-word commands won''t work right now... a is only one word
+            item = cleanstr(' '.join(rend[2:len(rend)]))
+        if db.select('name', 'items', 'name', item) != None:
+            return (True, item.give(item, inventory, id, position, cleanstr(rend[1])))
+    elif (rend[0] == 'inventory') or (cmd == 'check inventory') or (cmd == 'what am i holding'):
         if inventory == {}:
             return (True, 'Your inventory is empty at the moment.')
         else:
             return (True, invbuild(inventory))
-    elif (cmdtweet == 'delete me from lilt') or (a == u'💀💀💀') or (cmdtweet == u'💀💀💀'):
+    elif (cmd == 'delete me from lilt') or (a == u'💀💀💀'):
         db.delete('users', 'id', id)
         return (True, 'You\'ve been removed from Lilt. Thanks for playing!')
-    elif (a == 'liltadd') and ((id == '15332057') or (id == '724754312757272576') or (id == '15332062')):
-        if len((b).split('~')) >= 2:
-            addmove, addresponse = (b).split('~',1)
+    elif (rend[0] == 'liltadd') and ((id == '15332057') or (id == '724754312757272576') or (id == '15332062')):
+        dbrend = rend[1].split('~')
+        print dbrend
+        if len(rend[1].split('~')) >= 2:
+            addmove, addresponse = rend[1].split('~',1)
             if addmove == 'item':
                 # liltadd item~n|paste~m|10
                 traits = dict(trait.split('|') for trait in (addresponse).split('~'))
