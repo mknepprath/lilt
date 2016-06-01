2f7806d152ddb4b50e2396b5d67d19032e01c461
Michael Knepprath <michael@mknepprath.com>
2016-06-01 16:57:32 -0500

    adding all clock stuff so lilt can respond faster

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python bot.py
+clock: python clock.py
---
+++ clock.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    execfile('bot.py')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    execfile('bot.py')
+
+sched.start()
