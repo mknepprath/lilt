036250eb4a414007d23b70f52ef671c5491d2e38
Michael Knepprath <michael@mknepprath.com>
2016-06-01 17:07:22 -0500

    pushed updates to clock.py

--- clock.py
+++ clock.py
@@ -1,15 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import logging
 
-logging.basicConfig()
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    execfile('bot.py')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
     execfile('bot.py')
 
 sched.start()
